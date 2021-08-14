a7c3534f076c7f4d4b812c07843bdb88ab14af6b
handsomevang <legithandsomevang@gmail.com>
2021-08-13 20:05:45 -0400

    finished creating model templates

---
+++ bready2deliver/bready2deliver/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bready2deliver project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bready2deliver.settings')
+
+application = get_wsgi_application()
